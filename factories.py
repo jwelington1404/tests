d81dbf1ec0fb1a8ed9201542787082af0c250e07
jwelington1404 <91572605+jwelington1404@users.noreply.github.com>
2024-09-09 18:04:39 -0300

    Create factories.py

---
+++ factories.py
@@ -0,0 +1,18 @@
+import factory
+from myapp.models import Product
+
+class ProductFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Product
+        sqlalchemy_session = db_session  # Use a sessão do banco de dados para testes
+
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker('word')
+    category = factory.Faker('word')
+    available = factory.Faker('boolean')
+    price = factory.Faker('pydecimal', left_digits=5, right_digits=2, positive=True)
+
+def test_product_factory(db_session):
+    product = ProductFactory()
+    assert product is not None
+    assert isinstance(product.name, str)
